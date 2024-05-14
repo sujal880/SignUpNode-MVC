d31b0570c0a5b4109b8bb450a5682adff194ecb5
sujal880 <sujaldave880@gmail.com>
2024-05-14 13:46:12 +0530

    Final Commit

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const app=express();
 app.use(express.json());
 
 app.use(allroutes);
-
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        message:"Welcome To Sujal's Api"
+    })
+})
 app.listen(5000, ()=>{
     console.log(`Server is Running on Port ${5000}`);
 })
